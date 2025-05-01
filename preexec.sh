077d3cad05b5e3a2a152bc121240b4b5b11048f0
Thom Neale <tneale@mitre.org>
2025-05-01 16:29:53 -0400

    updates

--- preexec.sh
+++ preexec.sh
@@ -1,14 +1,14 @@
 # see https://github.com/rcaloras/bash-preexec
 function preexec() { 
   export ___PREEXEC_CMD="$(echo -n $1 | base64)"; 
-  export ___PREEXEC_START_TIMESTAMP="$(date --rfc-3339=ns)"; 
+  export ___PREEXEC_START_TIMESTAMP="$(gdate --rfc-3339=ns)"; 
 }
 
 function precmd () {
   ($TOTAL_RECALL_ROOT/cli \
       -command="$___PREEXEC_CMD" \
       -return-code="$?" \
-      -start-timestamp="$___PREEXEC_START_TIMESTAMP" -end-timestamp="$(date --rfc-3339=ns)"  &); 
+      -start-timestamp="$___PREEXEC_START_TIMESTAMP" -end-timestamp="$(gdate --rfc-3339=ns)"  &); 
   unset ___PREEXEC_CMD;
   unset ___PREEXEC_START_TIMESTAMP;
 }
